6b5cc08362fe0f1bb141ad879bfaba4c5ff5e64e
Pascal Betting <pascal.betting@gmx.de>
2021-01-09 20:50:04 +0100

    feat: support opening intellij or vscode
    + display different icons for the tools which will open the directory

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 class RepoOpenerExtension(Extension):
 
+    tool_command_map = {
+        'code': 'code-insiders',
+        'intellij': 'intellij-idea-ultimate'
+    }
+
     root_path = "$HOME/repos"
 
     def __init__(self):
@@ -28,14 +33,21 @@ class RepoOpenerExtension(Extension):
 
     def find_local_git_repos(self):
         root_path = self.root_path
-        repos = [{"path": folder_entry[0], "tool": "code-insiders"} for folder_entry in os.walk(
+        repos = [{"path": folder_entry[0], "subdirs": folder_entry[1]} for folder_entry in os.walk(
             os.path.expandvars(root_path)) if ".git" in folder_entry[1]]
+
+        for repo in repos:
+            tool = "code"
+            if ".idea" in repo["subdirs"]:
+                tool = "intellij"
+
+            repo["tool"] = tool
 
         return repos
 
     def open_repo(self, repo):
         path = repo["path"]
-        tool = repo["tool"]
+        tool = self.tool_command_map[repo["tool"]]
 
         subprocess.Popen([tool, path])
 
@@ -50,9 +62,10 @@ class KeywordQueryEventListener(EventListener):
     def gen_repo_item(self, repo):
         path = repo["path"]
         name = os.path.basename(path)
+        icon = repo["tool"]
         description = "Open me!"
         open_action = ExtensionCustomAction({"action": "open", "repo": repo})
-        return ExtensionResultItem(icon="images/icon.png", name=name, description=description, on_enter=open_action)
+        return ExtensionResultItem(icon=f"images/{icon}.png", name=name, description=description, on_enter=open_action)
 
 
 class ItemEnterEventListener(EventListener):
