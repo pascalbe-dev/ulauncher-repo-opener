a025c0ddf0bac1336477b66bc22488db318ed8a5
Lukas Kürzdörfer <luk@vfm-gruppe.de>
2024-12-05 11:41:14 +0100

    add more editors, detect editor by language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
@@ -13,19 +14,85 @@ from ulauncher.api.shared.event import (ItemEnterEvent, KeywordQueryEvent,
                                         PreferencesUpdateEvent)
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
+class CodeEditor(str):
+    CODE = "code"
+    WEBSTORM = "webstorm"
+    PYCHARM = "pycharm"
+    INTELLIJ = "intellij"
+    GOLAND = "goland"
+    RUSTROVER = "rustrover"
+    RIDER = "rider"
+
+class CodeEditorResolver:
+    DEFAULT_EDITOR = CodeEditor.CODE
+
+    LANGUAGE_EDITOR_MAP = {
+        'python': CodeEditor.PYCHARM,
+        'javascript': CodeEditor.WEBSTORM,
+        'java': CodeEditor.INTELLIJ,
+        'go': CodeEditor.GOLAND,
+        'rust': CodeEditor.RUSTROVER,
+        'csharp': CodeEditor.RIDER,
+    }
+
+    LANGUAGE_FILE_EXTENSIONS = {
+        'python': ['.py'],
+        'javascript': ['.js', '.jsx', '.ts', '.tsx'],
+        'java': ['.java', '.kt', '.kts'],
+        'csharp': ['.cs', '.sln'],
+        'go': ['.go'],
+        'rust': ['.rs'],
+    }
+
+    @staticmethod
+    def detect_language(folder_path: str) -> set:
+        language_counts = {language: 0 for language in CodeEditorResolver.LANGUAGE_FILE_EXTENSIONS}
+
+        for _, _, files in os.walk(folder_path):
+            for file in files:
+                _, ext = os.path.splitext(file)
+                for language, extensions in CodeEditorResolver.LANGUAGE_FILE_EXTENSIONS.items():
+                    if ext in extensions:
+                        language_counts[language] += 1
+
+        languages_with_more_than_10_files = [lang for lang, count in language_counts.items() if count > 10]
+
+        if len(languages_with_more_than_10_files) > 1:
+            return "unknown"
+        
+        most_common_language = max(language_counts, key=language_counts.get, default=None)
+
+        return most_common_language if language_counts[most_common_language] > 0 else "unknown"
+
+    @staticmethod
+    def get_editor(folder_path: str) -> str:
+        language = CodeEditorResolver.detect_language(folder_path)
+        editor = CodeEditorResolver.LANGUAGE_EDITOR_MAP[language] if language in CodeEditorResolver.LANGUAGE_EDITOR_MAP else CodeEditorResolver.DEFAULT_EDITOR
+        return editor
 
 class RepoOpenerExtension(Extension):
     tool_command_map = {
         'code': None,
-        'intellij': None
+        'intellij': None,
+        'rider': None,
+        'pycharm': None,
+        'webstorm': None,
+        'goland': None,
+        'rustrover': None,
     }
 
     shorthand_tool_map = {
         'c': 'code',
-        'i': 'intellij'
+        'i': 'intellij',
+        'r': 'rider',
+        'p': 'pycharm',
+        'w': 'webstorm',
+        'g': 'goland',
+        'rr': 'rustrover'
     }
 
     search_path = None
+    mono_repositories: str = None
 
     repos = []
 
@@ -37,16 +104,44 @@ class RepoOpenerExtension(Extension):
                        PreferencesUpdateEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
+    def resolve_installed_tools(self):
+        for tool, path in self.tool_command_map.items():
+            self.tool_command_map[tool] = shutil.which(path if path is not None else tool)
+
     def find_and_store_local_git_repos(self):
         root_path = self.search_path
         repos = [{"path": folder_entry[0],
-                  "subdirs": folder_entry[1],
                   "name": os.path.basename(folder_entry[0]),
-                  "tool": "intellij" if ".idea" in folder_entry[1] else "code"
+                  "tool": self.get_editor(folder_entry[0])
                   }
                  for folder_entry in os.walk(os.path.expandvars(root_path)) if ".git" in folder_entry[1]]
+        
+        if self.mono_repositories:
+            for m in [x for x in self.mono_repositories.split(';') if x]:
+                parts = m.split(':')
+                if len(parts) != 2:
+                    continue
+                repo_name = parts[0]
+                repo = next((repo for repo in repos if repo["name"].lower() == repo_name.lower()), None)
+                if repo is None:
+                    continue
+                for f in [x for x in parts[1].split(',') if x]:
+                    folder_path = os.path.join(repo["path"], f)
+                    if os.path.isdir(folder_path):
+                        repos.append({
+                            "path": folder_path,
+                            "name": repo["name"] + " - " + f,
+                            "tool": self.get_editor(folder_path)
+                        })
 
         self.repos = repos
+
+    def get_editor(self, folder_path: str) -> str:
+        editor = CodeEditorResolver.get_editor(folder_path)
+        if self.tool_command_map[editor] is not None:
+            return editor
+        return CodeEditorResolver.DEFAULT_EDITOR
+
 
     def open_repo(self, repo):
         path = repo["path"]
@@ -82,6 +177,7 @@ class KeywordQueryEventListener(EventListener):
 
         repos = [repo.copy() for repo in extension.repos if search_term.lower()
                  in repo["name"].lower()]
+
 
         if has_custom_tool:
             for repo in repos:
@@ -121,6 +217,7 @@ class ItemEnterEventListener(EventListener):
         action = event_data["action"]
 
         if action == "refresh":
+            extension.resolve_installed_tools()
             extension.find_and_store_local_git_repos()
         elif action == "open":
             repo = event_data["repo"]
@@ -132,20 +229,38 @@ class PreferencesEventListener(EventListener):
         extension.search_path = event.preferences["search_path"]
         extension.tool_command_map["code"] = event.preferences["vscode_command"]
         extension.tool_command_map["intellij"] = event.preferences["intellij_command"]
+        extension.tool_command_map["rider"] = event.preferences["rider_command"]
+        extension.tool_command_map["pycharm"] = event.preferences["pycharm_command"]
+        extension.tool_command_map["webstrom"] = event.preferences["webstrom_command"]
+        extension.tool_command_map["goland"] = event.preferences["goland_command"]
+        extension.tool_command_map["rustrover"] = event.preferences["rustrover_command"]
+        extension.mono_repositories = event.preferences["mono_repositories"]
+        extension.resolve_installed_tools()
         extension.find_and_store_local_git_repos()
 
 
 class PreferencesUpdateEventListener(EventListener):
     def on_event(self, event, extension: RepoOpenerExtension):
-        if event.id == "search_path":
+        if event.id == "search_path" or "mono_repositories":
             extension.search_path = event.new_value
             extension.find_and_store_local_git_repos()
-        elif event.id == "vscode_command":
+            return
+        if event.id == "vscode_command":
             extension.tool_command_map["code"] = event.new_value
         elif event.id == "intellij_command":
             extension.tool_command_map["intellij"] = event.new_value
-        else:
-            pass
+        elif event.id == "rider_command":
+            extension.tool_command_map["rider"] = event.new_value
+        elif event.id == "webstrom_command":
+            extension.tool_command_map["webstrom"] = event.new_value
+        elif event.id == "pycharm_command":
+            extension.tool_command_map["pycharm"] = event.new_value
+        elif event.id == "goland_command":
+            extension.tool_command_map["goland"] = event.new_value
+        elif event.id == "rustrover_command":
+            extension.tool_command_map["rustrover"] = event.new_value
+        
+        extension.resolve_installed_tools()
 
 
 if __name__ == "__main__":
